48ceb3125016c477384ac0fd844c738e73def163
henriqueberlesi <henrique.berlesi@gmail.com>
2018-11-08 15:44:14 +0000

    add LAB12

---
+++ entrega_programada_12/EP12-C.c
@@ -0,0 +1,27 @@
+/* Entrega Programada 12 – C
+
+Descrição: Média Aparada
+
+Faça um programa para calcular a média aparada das notas reais
+de 15 alunos. A média aparada deve ser calculada descartando a
+maior e a menor nota. Para isso, crie uma função que localize e
+retorne o maior valor e uma outra função para localizar e retornar o
+menor valor de um vetor. Então calcule a média aparada. A média
+deve ser impressa com duas casas decimais.
+
+Dicas:
+Podemos fazer 3 funções.
+
+- MenorValor() que recebe o vetor e retorna o menor valor contido nele.
+
+- MaiorValor() que recebe o vetor e retorna o maior valor contido nele.
+
+- MediaAparada(), que recebe o vetor, chama as duas outras funções para descobrir
+quais são os valores extremos, calcula e retorna a média aparada.
+
+Se houverem várias notas empatadas como maior ou menor nota deve-se
+desconsiderar todas.
+
+Atenção:
+O programa deverá obrigatoriamente usar funções. */
+
